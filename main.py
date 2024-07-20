3143a845129b42635c349493e836b0fa71e95178
Kowalsky <106312642+Kowalskyy@users.noreply.github.com>
2024-07-20 20:55:22 +0300

    some qol's
    adequate naming, removed "one-lined-vars"

--- main.py
+++ main.py
@@ -17,75 +17,97 @@ bot = Client('bot', api_id, api_hash, bot_token=token)
 
 
 @bot.on_message(filters.command('prefix', '/') & filters.group)
-async def prfx_hndl(client, msg: types.Message):
+async def prefix(_, msg: types.Message):
 	await msg.delete()
-	un = msg.from_user.username
-	cid = msg.chat.id
-	user = await bot.get_chat_member(cid, un)
-	arg = msg.text.split(maxsplit=1)
-	prfx = arg[1] if not len(arg) <= 1 else ''
-	
+	username = msg.from_user.username
+	chat_id = msg.chat.id
+	user = await bot.get_chat_member(chat_id, username)
+	prefix = msg.text.split(maxsplit=1)
+
+	if len(prefix) >= 1:
+		prefix = prefix[1]
+	else:
+		prefix = ''
+
 	if not user.status == enums.ChatMemberStatus.ADMINISTRATOR:
 		print(user.status)
-		await bot.send_message(cid, 'У вас нет префикса (ну, или вы владелец или вы бот).')
+		await bot.send_message(chat_id, 'У вас нет префикса (ну, или вы владелец или вы бот).')
 		print(f'@{user.user.username} has no prefix')
 		return
 	if not user.promoted_by.username == bot_name:
-		await bot.send_message(cid, f'Ваш префикс выдан другим [человеком](https://t.me/{user.promoted_by.username}). Извините, я не могу его поменять.', disable_web_page_preview=True)
+		await bot.send_message(chat_id, f'Ваш префикс выдан другим [человеком](https://t.me/{user.promoted_by.username}). Извините, я не могу его поменять.', disable_web_page_preview=True)
 		print(f'@{user.user.username} prefix is given by other person @{user.promoted_by.username}')
 		return
-	if len(prfx) > 16:
-		await bot.send_message(cid, 'Длина префикса больше 16 символов.')
-		print(f'prefix is {len(prfx)} long')
+	if len(prefix) > 16:
+		await bot.send_message(chat_id, 'Длина префикса больше 16 символов.')
+		print(f'prefix is {len(prefix)} long')
 		return
 	
-	await bot.set_administrator_title(cid, un, prfx)
-	await bot.send_message(cid, f'Префикс сменен с "{user.custom_title}" на "{prfx}"')
-	print(f'changed @{un} from {user.custom_title} to {prfx}')
+	await bot.set_administrator_title(chat_id, username, prefix)
+	await bot.send_message(chat_id, f'Префикс сменен с "{user.custom_title}" на "{prefix}"')
+	print(f'changed @{username} from {user.custom_title} to {prefix}')
 	
 @bot.on_message(filters.command('addprefix', '/') &  filters.group & filters.user(allowed_users))
-async def aprf_hndl(client, msg: types.Message):
+async def add_prefix(_, msg: types.Message):
 	await msg.delete()
-	cid = msg.chat.id
+	chat_id = msg.chat.id
 	arg = msg.text.split(maxsplit=2)
 	users = []
-	async for x in bot.get_chat_members(cid): #have no clue how to make it easier
+	async for x in bot.get_chat_members(chat_id): #have no clue how to make it easier
 		users.append(f"@{x.user.username}")
-	user = arg[1] if len(arg) > 1 and arg[1].startswith("@") else f"@{msg.reply_to_message.from_user.username}" if msg.reply_to_message else None
-	prfx = arg[2] if len(arg) > 2 else arg[1] if len(arg) > 1 and not arg[1].startswith("@") else ''
+
+	if len(arg) > 1 and arg[1].startswith("@"):
+		user = arg[1]
+	elif msg.reply_to_message:
+		user = f'@{msg.reply_to_message.from_user.username}'
+	else:
+		user = None
 	
+	if len(arg) > 2:
+		prefix = arg[2]
+	elif len(arg) > 1 and not arg[1].startswith("@"):
+		prefix = arg[1]
+	else:
+		prefix = ''
+
 	if not user in users:
-		await bot.send_message(cid, f'Этого пользователя нет в беседе.')
+		await bot.send_message(chat_id, f'Этого пользователя нет в беседе.')
 		print(f'{user} user not found')
 		return
 	if user is None:
-		await bot.send_message(cid, f'Пожалуйста, укажите пользователя при ответе или укажите его тег.')
+		await bot.send_message(chat_id, f'Пожалуйста, укажите пользователя при ответе или укажите его тег.')
 		print('no user specified')
 		return
-	if len(prfx) > 16:
-		await bot.send_message(cid, 'Длина префикса больше 16 символов.')
-		print(f'prefix is {len(prfx)} long')
+	if len(prefix) > 16:
+		await bot.send_message(chat_id, 'Длина префикса больше 16 символов.')
+		print(f'prefix is {len(prefix)} long')
 		return
 	
-	await bot.promote_chat_member(cid, user)
-	await bot.set_administrator_title(cid, user, prfx)
-	await bot.send_message(cid, 'Префикс выдан.')
-	print(f'added {prfx} to {user}')
+	await bot.promote_chat_member(chat_id, user)
+	await bot.set_administrator_title(chat_id, user, prefix)
+	await bot.send_message(chat_id, 'Префикс выдан.')
+	print(f'added {prefix} to {user}')
 
 @bot.on_message(filters.command('delprefix', '/') & filters.group & filters.user(allowed_users))
-async def dprf_hndl(client, msg: types.Message):
+async def delete_prefix(_, msg: types.Message):
 	await msg.delete()
-	cid = msg.chat.id
-	arg = msg.text.split(maxsplit=1)
-	user = arg[1] if len(arg) > 1 and arg[1].startswith("@") else f"@{msg.reply_to_message.from_user.username}" if msg.reply_to_message else None
+	chat_id = msg.chat.id
+	user = msg.text.split(maxsplit=1)
+
+	if len(user) > 1 and user[1].startswith("@"):
+		user = user[1]
+	elif msg.reply_to_message:
+		user = f"@{msg.reply_to_message.from_user.username}"
+	else:
+		user = None
 
 	if user is None:
-		await bot.send_message(cid, 'Пожалуйста, укажите пользователя при ответе или укажите его тег.')
+		await bot.send_message(chat_id, 'Пожалуйста, укажите пользователя при ответе или укажите его тег.')
 		print('no user specified')
 		return
 	
-	await bot.promote_chat_member(cid, user, types.ChatPrivileges(can_manage_chat=False))
-	await bot.send_message(cid, 'Префикс удален.')
+	await bot.promote_chat_member(chat_id, user, types.ChatPrivileges(can_manage_chat=False))
+	await bot.send_message(chat_id, 'Префикс удален.')
 	print(f'deleted {user} prefix')
 
 if __name__ == '__main__':
