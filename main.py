ffeace88b8ec65808032c1357d6e0084c9a3996a
Kowalsky <106312642+Kowalskyy@users.noreply.github.com>
2024-10-15 22:12:03 +0300

    fully rewritten code

--- main.py
+++ main.py
@@ -1,114 +1,123 @@
-import asyncio
-from pyrogram import Client, filters, types, enums
-
+from pyrogram import Client, types, filters, enums
+from pyrogram.errors import bad_request_400 as err
 
 # how to get api_id/hash - https://core.telegram.org/api/obtaining_api_id
 # how to get bot token - https://core.telegram.org/bots/tutorial#obtain-your-bot-token
 # in bot_name type bot username without @
 # in allowed_users type users username (also without @) or ids who will have access for /addprefix and /delprefix
 
-#note/ there is no handlers to adding/deleting prefixes who wasn't promoted by bot, so it'll error eventually
-api_id = int
-api_hash = str
-token = str
-bot_name = str
-allowed_users = []
-bot = Client('bot', api_id, api_hash, bot_token=token)
+bot_token = ''
+api_id = ''
+api_hash = ''
 
+bot = Client('bot', api_id, api_hash, bot_token=bot_token)
+users = ['@durov'] #users that will have access to /addprefix and /delprefix
 
-@bot.on_message(filters.command('prefix', '/') & filters.group)
-async def prefix(_, msg: types.Message): #custom title change
-	await msg.delete()
-	username = msg.from_user.username
-	chat_id = msg.chat.id
-	user = await bot.get_chat_member(chat_id, username)
-	prefix = msg.text.split(maxsplit=1)
+@bot.on_message(filters.command('prefix') & filters.group)
+async def prefix_handler(_, msg:types.Message):
+    await msg.delete()
 
-	if len(prefix) >= 1: #check if there is anything after /prefix
-		prefix = prefix[1] #changes to whatever user typed if present
-	else:
-		prefix = '' #changes to blank, will delete custom title
+    chat = msg.chat.id
+    user = await bot.get_chat_member(chat, msg.from_user.id)
+    bot_id = await bot.get_me() #using it just to get bot id to check if member was promoted by bot
+    prefix = msg.text.removeprefix('/prefix').removeprefix(' ') #second .removeprefix to make it "title" and not " title"
 
-	if not user.status == enums.ChatMemberStatus.ADMINISTRATOR: #check if user have any admin rights
-		print(user.status)
-		await bot.send_message(chat_id, 'У вас нет префикса (ну, или вы владелец или вы бот).')
-		print(f'@{user.user.username} has no prefix')
-		return
-	if not user.promoted_by.username == bot_name: #check if user was promoted by other user
-		await bot.send_message(chat_id, f'Ваш префикс выдан другим [человеком](https://t.me/{user.promoted_by.username}). Извините, я не могу его поменять.', disable_web_page_preview=True)
-		print(f'@{user.user.username} prefix is given by other person @{user.promoted_by.username}')
-		return
-	if len(prefix) > 16: #check if custom title lenght is less than 16 characters
-		await bot.send_message(chat_id, 'Длина префикса больше 16 символов.')
-		print(f'prefix is {len(prefix)} long')
-		return
-	
-	await bot.set_administrator_title(chat_id, username, prefix)
-	await bot.send_message(chat_id, f'Префикс сменен с "{user.custom_title}" на "{prefix}"')
-	print(f'changed @{username} from {user.custom_title} to {prefix}')
-	
-@bot.on_message(filters.command('addprefix', '/') &  filters.group & filters.user(allowed_users))
-async def add_prefix(_, msg: types.Message): #adds admin rights to user and custom title if present
-	await msg.delete()
-	chat_id = msg.chat.id
-	arg = msg.text.split(maxsplit=2)
-	users = []
-	async for x in bot.get_chat_members(chat_id): #gets every user in chat
-		users.append(f"@{x.user.username}") #i have no clue how to make it easier
+    if user.status == enums.ChatMemberStatus.MEMBER:
+        await bot.send_message(chat, 'У вас нет префикса.')
+        print(f'{user.user.first_name} does not have prefix')
+        return
+    
+    if user.promoted_by.id != bot_id.id:
+        await bot.send_message(chat, 'Префикс нельзя сменить, потому что он выдан другим пользователем.')
+        print(f'can not change {user.user.first_name} prefix because he was promoted by {user.promoted_by.id}')
+        return
 
-	if len(arg) > 1 and arg[1].startswith("@"):
-		user = arg[1] #changes to @tag if user typed [/addprefix @tag prefix]
-	elif msg.reply_to_message:
-		user = f'@{msg.reply_to_message.from_user.username}' #changes to @tag if user used command with reply like [reply /addprefix prefix]
-	else:
-		user = None #changes to none in case user isn't present in command
-	
-	if len(arg) > 2:
-		prefix = arg[2] #changes to prefix if user typed [/addprefix @tag prefix]
-	elif len(arg) > 1 and not arg[1].startswith("@"):
-		prefix = arg[1] #changes to prefix if user used command with reply like [reply /addprefix prefix]
-	else:
-		prefix = '' #changes to blank just to give user very basic rights
+    if len(prefix) > 16:
+        await bot.send_message(chat, 'Ваш префикс более 16 символов в длину.')
+        print(f'can not change {user.user.first_name} prefix because its lenght more than 16 ({len(prefix)})')
+        return
 
-	if not user in users: #check if user is present in chat
-		await bot.send_message(chat_id, f'Этого пользователя нет в беседе.')
-		print(f'{user} user not found')
-		return
-	if user is None: #check if user wasn't present in command
-		await bot.send_message(chat_id, f'Пожалуйста, укажите пользователя при ответе или укажите его тег.')
-		print('no user specified')
-		return
-	if len(prefix) > 16: #check if custom title lenght is less than 16 characters
-		await bot.send_message(chat_id, 'Длина префикса больше 16 символов.')
-		print(f'prefix is {len(prefix)} long')
-		return
-	
-	await bot.promote_chat_member(chat_id, user)
-	await bot.set_administrator_title(chat_id, user, prefix)
-	await bot.send_message(chat_id, 'Префикс выдан.')
-	print(f'added {prefix} to {user}')
+    await bot.set_administrator_title(chat, user.user.id, prefix)
+    await bot.send_message(chat, f'Префикс успешно сменен с "{user.custom_title}" на "{prefix}"')
+    print(f'successfully changed {user.user.first_name} prefix from {user.custom_title} to {prefix}')
 
-@bot.on_message(filters.command('delprefix', '/') & filters.group & filters.user(allowed_users))
-async def delete_prefix(_, msg: types.Message): #deletes admin rights from user
-	await msg.delete()
-	chat_id = msg.chat.id
-	user = msg.text.split(maxsplit=1)
+@bot.on_message(filters.command('addprefix') & filters.group & filters.user(users))
+async def addprefix_handler(_, msg:types.Message):
+    await msg.delete()
 
-	if len(user) > 1 and user[1].startswith("@"):
-		user = user[1] #changes to @tag if user typed [/delprefix @tag]
-	elif msg.reply_to_message:
-		user = f"@{msg.reply_to_message.from_user.username}" #changes to @tag if user used command with reply like [reply /delprefix]
-	else:
-		user = None #changes to none in case user isn't present in command
+    prefix = msg.text.removeprefix('/addprefix').removeprefix(' ')
+    chat = msg.chat.id
 
-	if user is None: #check if user wasn't present in command
-		await bot.send_message(chat_id, 'Пожалуйста, укажите пользователя при ответе или укажите его тег.')
-		print('no user specified')
-		return
-	
-	await bot.promote_chat_member(chat_id, user, types.ChatPrivileges(can_manage_chat=False))
-	await bot.send_message(chat_id, 'Префикс удален.')
-	print(f'deleted {user} prefix')
+    if not msg.reply_to_message is None: #in case if command used like reply /addprefix title
+        username = msg.reply_to_message.from_user.id
+    if prefix.startswith('@'): #in case if command used like /addprefix @username title
+        if len(prefix.split(maxsplit=1)) > 1: #sometimes people just wanna add right to user
+            username, prefix = prefix.split(maxsplit=1)
+        else:
+            username = prefix
+            prefix = ''
+
+    try:
+        user = await bot.get_chat_member(chat, username)
+    except err.UserNotParticipant:
+        await bot.send_message(chat, 'Этого пользователя нет в беседе.')
+        print('user is not presented in chat')
+        return
+    except err.UsernameNotOccupied:
+        await bot.send_message(chat, 'Этого пользователя не существует.')
+        print('user does not exist')
+        return
+
+    if user.status != enums.ChatMemberStatus.MEMBER: #this checks should be upper, ikr?
+        await bot.send_message(chat, 'У этого человека уже есть префикс.')
+        print(f'can not add prefix to {user.user.username} because he is not member (admin, owner, etc)')
+        return
+    if len(prefix) > 16:
+        await bot.send_message(chat, 'Выдаваемый префикс более 16 символов в длину.')
+        print(f'can not add prefix to {user.user.username} because its lenght more than 16 ({len(prefix)})')
+        return
+
+    await bot.promote_chat_member(chat, user.user.id)
+    await bot.set_administrator_title(chat, user.user.id, prefix)
+    await bot.send_message(chat, f'Префикс "{prefix}" выдан пользователю {user.user.first_name}.')
+    print(f'successfully added "{prefix} to a {user.user.first_name}')
+
+@bot.on_message(filters.command('delprefix') & filters.group & filters.user(users))
+async def delprefix_handler(_, msg:types.Message):
+    await msg.delete()
+
+    prefix = msg.text.removeprefix('/delprefix').removeprefix(' ')
+    bot_id = await bot.get_me()
+    chat = msg.chat.id
+
+    if not msg.reply_to_message is None:
+        username = msg.reply_to_message.from_user.id
+    if prefix.startswith('@'):
+        username = prefix
+
+    try:
+        user = await bot.get_chat_member(chat, username)
+    except err.UserNotParticipant:
+        await bot.send_message(chat, 'Этого пользователя нет в беседе.')
+        print('user is not presented in chat')
+        return
+    except err.UsernameNotOccupied:
+        await bot.send_message(chat, 'Этого пользователя не существует.')
+        print('user does not exist')
+        return
+
+    if user.status == enums.ChatMemberStatus.MEMBER:
+        await bot.send_message(chat, 'У этого человека нет префикса.')
+        print(f'can not delete prefix from {user.user.username} because he is member already')
+        return
+    if user.promoted_by.id != bot_id.id:
+        await bot.send_message(chat, 'Префикс нельзя удалить, потому что он выдан другим пользователем.')
+        print(f'can not delete prefox from {user.user.username} because he was promoted by {user.promoted_by.id}')
+        return
+
+    await bot.promote_chat_member(chat, user.user.id, types.ChatPrivileges(can_manage_chat=False))
+    await bot.send_message(chat, f'Префикс пользователя {user.user.first_name} удален.')
+    print(f'successfully deleted prefix from {user.user.first_name}')
 
 if __name__ == '__main__':
-	bot.run()
+    bot.run()
